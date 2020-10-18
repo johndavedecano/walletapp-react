3ab2323510b75dbe6106aabda9b6052db65cf1dd
John Dave Decano <johndavedecano@gmail.com>
2020-10-18 15:23:16 +0800

    added loadable

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react'
 
+import Loadable from 'react-loadable'
+
 import { Switch, Route } from 'wouter'
 
 import { AppStateProvider } from 'AppContext'
 
 import AppLayout from 'components/Layouts/AppLayout'
-import Home from 'pages/home'
-import Login from 'pages/login'
-import NotFound from 'pages/notfound'
-import Register from 'pages/register'
 
 import './tailwind.output.css'
+
+const Loading = () => <div>Loading...</div>
 
 function App() {
   return (
     <AppStateProvider>
       <AppLayout>
         <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="" component={NotFound} />
+          <Route
+            exact
+            path="/"
+            component={Loadable({
+              loader: () => import('pages/login'),
+              loading: Loading,
+            })}
+          />
+          <Route
+            exact
+            path="/register"
+            component={Loadable({
+              loader: () => import('pages/register'),
+              loading: Loading,
+            })}
+          />
+          <Route
+            exact
+            path="/home"
+            component={Loadable({
+              loader: () => import('pages/home'),
+              loading: Loading,
+            })}
+          />
+          <Route
+            exact
+            path=""
+            component={Loadable({
+              loader: () => import('./pages/notfound'),
+              loading: Loading,
+            })}
+          />
         </Switch>
       </AppLayout>
     </AppStateProvider>
